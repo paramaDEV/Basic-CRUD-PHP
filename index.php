64921ba2366e54baad309d955f3b60ddacc048e4
paramaDEV <admaja404@gmail.com>
2020-10-31 23:45:47 +0700

    Initial commit

---
+++ index.php
@@ -0,0 +1,39 @@
+<html>
+    <head>
+        <title>CRUD Dasar</title>
+        <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@500&display=swap" rel="stylesheet">
+        <link rel="stylesheet" href="style.css">
+    </head>
+    <body>
+        <nav>
+            <h1>paramaDEV</h1>
+        </nav>
+    </body>
+    <div class="content">
+        <h2>Data Mahasiswa</h2>
+        <button class="add">Tambah Data</button>
+        <table border=1 cellpadding="20px" cellspacing="0px">
+            <tr>
+                <th>No</th>
+                <th>Nama</th>
+                <th>NIM</th>
+                <th>Kelamin</th>
+                <th>Fakultas</th>
+                <th>Jurusan</th>
+                <th>Actions</th>
+            </tr>
+            <tr>
+                <td>1</td>
+                <td>Fany Parama Admaja</td>
+                <td>18650063</td>
+                <td>Laki - Laki</td>
+                <td>Saintek</td>
+                <td>Teknik Informatika</td>
+                <td>
+                    <button class="edit">Edit</butto>
+                    <button class="delete">Hapus</butto>
+                </td>
+            </tr>
+        <table>
+    </div>
+</html>
